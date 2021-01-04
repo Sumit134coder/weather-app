e832c9d614b27823b34826ed964b1a889eb73b77
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-01-04 14:50:48 +0530

    fix : api fix, feat: cityName

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var cityName = document.querySelector("input");
 var Temp = document.querySelector("#temperature");
 var weather = document.querySelector("#weather");
 var btn = document.querySelector("button");
+var Name = document.querySelector("#name");
 var serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
-var key = "d1e095db37fecb6124dbf5aa10947009";
+var key = 'd1e095db37fecb6124dbf5aa10947009';
 function UpdateUrl(inputcity)
 {
     return serverUrl + "?" + "q=" + inputcity + "&"+"appid="+ key;
@@ -14,11 +15,12 @@ function ErrorHandler(error){
 function ClickHandler(){
     var inputcity = cityName.value;
     fetch(UpdateUrl(inputcity))
-    .then(Response => Response.json)
+    .then(Response => Response.json())
     .then(json =>{
         console.log(json)
-        Temp.innerHTML = json.main.temp;
-        weather.innerHTML = json.weather.description;
+        Name.innerHTML = json['name'];
+        Temp.innerHTML = parseFloat(json['main']['temp']) - 273.15;
+        weather.innerHTML = json['weather'][0]['description'];
     })
     .catch(ErrorHandler)
 }
