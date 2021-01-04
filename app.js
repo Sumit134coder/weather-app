29fe1bb1e6b2208ae16739cf7844b6106b819d1b
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-01-04 19:13:32 +0530

    style and feat:icons

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var btn = document.querySelector("button");
 var Name = document.querySelector("#name");
 var serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
 var key = 'd1e095db37fecb6124dbf5aa10947009';
+var icon = document.querySelector('img');
 function UpdateUrl(inputcity)
 {
     return serverUrl + "?" + "q=" + inputcity + "&"+"appid="+ key;
@@ -18,9 +19,11 @@ function ClickHandler(){
     .then(Response => Response.json())
     .then(json =>{
         console.log(json)
-        Name.innerHTML = json['name'];
-        Temp.innerHTML = parseFloat(json['main']['temp']) - 273.15;
-        weather.innerHTML = json['weather'][0]['description'];
+        Name.innerHTML = "AREA : "+ json['name'];
+        Temp.innerHTML = "TEMP : " + (parseInt(json['main']['temp']) - 273.15);
+        weather.innerHTML = "WEATHER : "+json['weather'][0]['description'];
+        var iconUrl = "http://openweathermap.org/img/wn/"+ json['weather'][0]['icon']+"@2x.png "
+        icon.src = iconUrl;
     })
     .catch(ErrorHandler)
 }
